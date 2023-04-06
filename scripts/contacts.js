458701ac8c1e8894dd4719215e0203092e731961
Workezy <109183071+Workezy@users.noreply.github.com>
2023-04-06 09:39:06 +0500

    Add files via upload

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,34 +1,39 @@
-(function () {
-  let modal = document.querySelector(".modal");
-  let closeButton = document.querySelector(".modalClose");
-  let modalTriggers = document.querySelectorAll("[data-trigger]");
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup__wrapper');
+// let popupWrapper = document.querySelector('.popup__wrapper')
+let openPopupButtons = document.querySelectorAll('.feedback');
+let closePopupButton = document.querySelector('.close-popup');
+let popupSubmit = document.querySelector('.popup__submit');
+let popupComplete = document.querySelector('.popup__complete');
 
-  let isModalOpen = false;
-  let pageYOffset = 0;
+openPopupButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupBg.classList.add('active');
+    popup.classList.add('active');
+  })
+});
 
-  let openModal = function () {
-    pageYOffset = window.pageYOffset;
-    modal.classList.add("is-open");
-    isModalOpen = true;
-  };
+closePopupButton.addEventListener('click', () => {
+  popupBg.classList.remove('active');
+  popup.classList.remove('active');
+});
 
-  let closeModal = function () {
-    modal.classList.remove("is-open");
-    isModalOpen = false;
-  };
+popupSubmit.addEventListener('click', () => {
+  popup.classList.remove('active');
+  popupComplete.classList.add('active');
+})
 
-  let onScroll = function (e) {
-    if (isModalOpen) {
-      e.preventDefault();
-      window.scrollTo(0, pageYOffset);
-    }
-  };
+document.addEventListener('click', (e) => {
+  if (e.target === popupBg) {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+    popupComplete.classList.remove('active');
+  }
+})
 
-  modalTriggers.forEach(function (item) {
-    item.addEventListener("click", openModal);
-  });
-
-  document.addEventListener("scroll", onScroll);
-
-  closeButton.addEventListener("click", closeModal);
-})();
+// function openSecondPopup () {
+//     popup.classList.remove('active');
+//     popupComplete.classList.add('active');
+// }
+ali
